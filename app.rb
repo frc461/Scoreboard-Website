780ab2488548c6eb05b685ba2cda244f15931f7c
Ian Wobschall <ian.wobschall@gmail.com>
2024-01-24 20:40:07 -0500

    shows match scores correctly as long as you don't refresh the page

--- app.rb
+++ app.rb
@@ -67,22 +67,31 @@ def fetch match_num #helper method to fetch match results and return rp from a m
     if match[:match_num].match(/\d+/)[0].to_i == match_num      
       match[:teams].each do |team|
         team[:rp] ||= alliance_data[color_to_number(team[:alliance])]['rp']
+        team[:score] = alliance_data[color_to_number(team[:alliance])]['totalPoints']
       end
       break
     end
   end
+  sort_matches
 end
 #variable["MatchScores"][0]["alliances"][color_to_number($matches[0][:teams][1])]['rp']
 
-def get_matches #[{match_num, teams: [{team_num, alliance, rp}]}]
+def get_matches #[{match_num, teams: [{team_num, alliance, rp, score}]}]
   request = HTTP.basic_auth(user: $username, pass: $password)
                 .headers('Content-Type': 'application/json')
                 .get("#{$base}/#{$season}/schedule/#{$event_key}?tournamentLevel=Qualification&teamNumber=461")
   JSON.parse(request.body)["Schedule"].each do |match| #the structure of the response is silly, but this will find the alliance color
     match_data = {match_num: "qm#{sprintf('%02d', match['matchNumber'])}", teams: []}
     match['teams'].each do |team|
-      match_data[:teams] << {team_num: team['teamNumber'], alliance: team['station'].match(/\D+/)[0], rp: false}
+      match_data[:teams] << {team_num: team['teamNumber'], alliance: team['station'].match(/\D+/)[0], rp: false, score: 0}
     end
     $matches << match_data
+  end
+  sort_matches
+end
+
+def sort_matches
+  $matches.each do |match|
+    match[:teams].sort!{ |a, b| a[:team_num] <=> b[:team_num] }.sort!{ |a, b| b[:alliance] <=> a[:alliance] }
   end
 end
